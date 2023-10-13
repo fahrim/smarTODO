8e87e2a578c3953a837c2f5a3332efc719db0c82
Fahri Meral <fahrimeral@outlook.com>
2023-10-13 23:49:59 +0300

    Add[db]: new todos table columns and migrate

--- database/migrations/2023_10_13_193114_create_todos_table.php
+++ database/migrations/2023_10_13_193114_create_todos_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable(); // title later on featured
+            $table->string('description');
+            $table->boolean('completed')->default(false);
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamp('due_date')->nullable();
+            // $table->foreignId('todo_list_id')->constrained()->cascadeOnDelete(); // todo_list_id later on featured
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
