5d639c12aa38d45f79a035053aec4d1d1af91970
Fahri Meral <fahrimeral@outlook.com>
2023-10-13 22:46:03 +0300

    Add: controller index func and main todos blade

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class TodoController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('todos', [
+            //
+        ]);
+    }
 }
