3b477b94c25cc57d7b91a67100baacdb568b8539
Fahri Meral <fahrimeral@outlook.com>
2023-10-13 23:11:09 +0300

    Add[step-1]: separate livewire blades in main todos

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -1,3 +1,34 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('My Todos') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl">
+
+                    <div class="text-gray-900 dark:text-gray-100">
+                      //  New Todo Form
+                    </div>
+                    {{-- <livewire:todos.new-todo-form />--}}
+
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl">
+
+                    <div class="text-gray-900 dark:text-gray-100">
+                        //  Todo List
+                    </div>
+                    {{-- <livewire:todos.todo-list />--}}
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
