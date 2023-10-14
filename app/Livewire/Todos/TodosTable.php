a50fbbf058e8fe202955a335131f5016551e94e1
Fahri Meral <fahrimeral@outlook.com>
2023-10-14 19:11:16 +0300

    Add[step-3-b]: todos table function ability list-paginate-sort

--- app/Livewire/Todos/TodosTable.php
+++ app/Livewire/Todos/TodosTable.php
@@ -3,11 +3,39 @@
 namespace App\Livewire\Todos;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodosTable extends Component
 {
+    use withPagination;
+
+    public $search = '';
+    public $sortField = 'created_at';
+    public $sortDirection = 'desc';
+    public $perPage = 4;
+
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortDirection = 'asc';
+        }
+
+        $this->sortField = $field;
+    }
+
+
     public function render()
     {
-        return view('livewire.todos.todos-table');
+        $todos = auth()->user()->todos()
+            ->where('description', 'like', '%'.$this->search.'%')
+            ->orderBy($this->sortField, $this->sortDirection)
+            ->paginate($this->perPage);
+
+        return view('livewire.todos.todos-table', [
+            'todos' => $todos,
+        ]);
     }
+
 }
--- resources/views/livewire/todos/todos-table.blade.php
+++ resources/views/livewire/todos/todos-table.blade.php
@@ -1,3 +1,16 @@
-<div class="text-gray-900 dark:text-gray-100">
-    //  Todos Table
+<div class="text-gray-900 dark:text-gray-100 space-y-6">
+    <div class="flex justify-between">
+        <button wire:click="sortBy('description')">Sort By Description</button>
+        <button wire:click="sortBy('created_at')">Sort By Created at</button>
+    </div>
+
+    @foreach($todos as $todo)
+        <div class="flex justify-between border-b pb-3">
+            <li class="w-3/4">{{ $todo->description }} <span class="flex items-center justify-end">{{ $todo->created_at->diffForHumans() }}</span></li>
+            <button class="border p-2" wire:click="delete">DELETE</button>
+        </div>
+
+    @endforeach
+
+    {{ $todos->links() }}
 </div>
