e53a313e51f3de3022330c1c5c7ced17f8a04cad
Fahri Meral <fahrimeral@outlook.com>
2023-10-16 03:28:09 +0300

    Feat: added `Todo Search` function

--- app/Livewire/Todos/TodosTable.php
+++ app/Livewire/Todos/TodosTable.php
@@ -3,7 +3,9 @@
 namespace App\Livewire\Todos;
 
 use App\Models\Todo;
+use Illuminate\View\View;
 use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,13 +13,49 @@ class TodosTable extends Component
 {
     use withPagination;
 
-    public $search = '';
-    public $sortField = 'created_at';
-    public $sortDirection = 'desc';
-    public $perPage = 10;
     public $pageTitle = 'My Todos Table';
 
-    public function sortBy($field)
+    #[Url(as:'q', history: true)]
+    public $search = '';
+
+    public $sortField = 'created_at';
+
+    public $sortDirection = 'desc';
+
+    public $perPage = 10;
+
+    protected function queryString()
+    {
+        return [
+            'search' => ['as' => 'q'],
+            'sortBy' => ['as' => 'sort'],
+            'sortDirection' => ['as' => 'direction'],
+        ];
+    }
+
+    #[On('todoAdded')]
+    public function updateTodosList(): void
+    {
+        $this->reset('this.todos');
+    }
+
+    public function getTodosProperty(): mixed
+    {
+        return auth()->user()->todos()
+            ->where('description', 'like', '%'.$this->search.'%')
+            ->orderBy($this->sortField, $this->sortDirection)
+            // ->simplePaginate($this->perPage);
+            ->paginate($this->perPage);
+    }
+
+    public function clearSearch(): void
+    {
+        $this->reset('search');
+        $this->reset('sortField');
+        $this->reset('sortDirection');
+    }
+
+    public function sortBy($field): void
     {
         if ($this->sortField === $field) {
             $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
@@ -28,29 +66,14 @@ class TodosTable extends Component
         $this->sortField = $field;
     }
 
-    #[On('todoAdded')]
-    public function updateTodosList()
-    {
-        $this->reset('this.todos');
-    }
-
-    public function getTodosProperty()
-    {
-        return auth()->user()->todos()
-            ->where('description', 'like', '%'.$this->search.'%')
-            ->orderBy($this->sortField, $this->sortDirection)
-            // ->simplePaginate($this->perPage);
-            ->paginate($this->perPage);
-    }
-
-    public function delete(Todo $todo)
+    public function delete(Todo $todo): void
     {
           $this->authorize('delete', $todo);
 
         $todo->delete();
     }
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.todos.todos-table', [
             'todos' => $this->getTodosProperty(),
--- resources/views/livewire/todos/todos-table.blade.php
+++ resources/views/livewire/todos/todos-table.blade.php
@@ -4,6 +4,16 @@
         <button wire:click="sortBy('created_at')">Sort By Created at</button>
     </div>
 
+    <div>
+        <x-input-label for="search" class="text-xs" :value="__('Search in your todos')" />
+        <div class="flex items-center">
+            <x-text-input wire:model.live="search" id="search" class="block mt-1 w-2/4 text-xs" type="text" name="search" autocomplete="Search" />
+            <x-secondary-button wire:click="clearSearch" class="ml-2">Clear</x-secondary-button>
+        </div>
+       <x-input-error :messages="$errors->get('search')" class="mt-2" />
+    </div>
+
+
     @foreach($todos as $todo)
         <div class="flex justify-between border-b pb-3">
             <li class="w-3/4">{{ $todo->description }} <span class="flex items-center justify-end">{{ $todo->created_at->diffForHumans() }}</span></li>
