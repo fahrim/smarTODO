478a9f917e021b3c76047e443df62f6d972f05f0
Fahri Meral <fahrimeral@outlook.com>
2023-10-19 21:42:35 +0300

    Reformat[ui]: chore necessary label tag

--- resources/views/components/input-checkbox.blade.php
+++ resources/views/components/input-checkbox.blade.php
@@ -1,4 +1,2 @@
 @props(['checked' => false])
-<label>
-    <input {{ $checked ? 'checked' : '' }} name="{!! $attributes->get('id') !!}" {!! $attributes->merge(['class' => 'rounded-lg dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 cursor-pointer']) !!}>
-</label>
+    <input type="checkbox" {{ $checked ? 'checked' : '' }} name="{!! $attributes->get('id') !!}" {!! $attributes->merge(['class' => 'rounded-lg dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800 cursor-pointer']) !!}>
