f87346fe32e4eb832dbfa44b493b4715a753289b
Fahri Meral <fahrimeral@outlook.com>
2023-10-26 23:57:27 +0300

    Install[ui]: add wireui blade components
    composer require wireui/wireui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [
+        require('./vendor/wireui/wireui/tailwind.config.js')
+    ],
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
