79e3c66943ac1fc995113abc9a4bcf7eea77630d
Fahri Meral <fahrimeral@outlook.com>
2023-10-27 20:02:07 +0300

    Fix[ui]: content col with resize

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -7,22 +7,13 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="max-w-xl mx-auto p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-
-                     <livewire:todos.new-todo-form />
-
-                </div>
+            <div class="max-w-2xl mx-auto p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <livewire:todos.new-todo-form/>
             </div>
 
-            <div class="max-w-xl mx-auto p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-
-                     <livewire:todos.todos-table />
-
-                </div>
+            <div class="max-w-2xl mx-auto p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <livewire:todos.todos-table/>
             </div>
         </div>
     </div>
-
 </x-app-layout>
